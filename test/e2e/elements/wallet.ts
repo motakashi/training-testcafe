dc779a60ef57ba5a2873152be559dd0463d68623
motakashi <takashi_home_sky@yahoo.co.jp>
2022-01-03 23:28:40 +0900

    金額指定ページのプルダウン表示が適切かのテストの追加

--- test/e2e/elements/wallet.ts
+++ test/e2e/elements/wallet.ts
@@ -3,4 +3,10 @@ import xpathSelector from "../lib/xpath-selector";
 
 export class Wallet {
   PJタイトル = Selector("#contents_header > h1").innerText;
+
+  金額指定プルダウン初期値 = Selector("#decision-money > select").child("option").withAttribute("selected").value;
+
+  金額指定プルダウン最小値 = Selector("#decision-money > select").child("option").nth(0).value;
+
+  金額指定プルダウン最大値 = Selector("#decision-money > select").child("option").nth(44).value;
 }
--- test/e2e/tests/wallet.test.ts
+++ test/e2e/tests/wallet.test.ts
@@ -17,3 +17,14 @@ export async function クレジットカードの金額指定ページにPJ名�
   const wallet = new Wallet();
   await t.expect(wallet.PJタイトル).eql(testData.projectName);
 }
+
+export async function クレジットカードの金額指定ページの初期金額が500円になっている() {
+  const wallet = new Wallet();
+  await t.expect(wallet.金額指定プルダウン初期値).eql("500");
+}
+
+export async function クレジットカードの金額指定ページのプルダウンメニューが最小値100円と最大値900万円になっている() {
+  const wallet = new Wallet();
+  await t.expect(wallet.金額指定プルダウン最小値).eql("100");
+  await t.expect(wallet.金額指定プルダウン最大値).eql("9000000");
+}
